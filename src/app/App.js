292f159649182b5838147ffb24cb6d5ad5247ad7
solomirrors <solomirrors@icloud.com>
2021-11-06 02:57:41 +0100

    refactor and test Fetch

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import './App.css';
+
+export default class App extends Component{
+  render() {
+    return (
+        <div>
+          <h4>Hello, New Project!</h4>
+        </div>
+    );
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./app";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+getResource('https://swapi.dev/api/people/1')
+    .then((body) =>{
+        console.log(body)
+    })
+
+ReactDOM.render(<App/>, document.getElementById('root'));
