99a1d6973c4477d79db5f16b7ab3beaa17c4d1ec
solomirrors <solomirrors@icloud.com>
2021-11-06 23:51:25 +0100

    random-planet.js/css

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -5,11 +5,39 @@ import './random-planet.css'
 const RandomPlanet = () => {
     return(
         <Container fluid className='p-4 pt-0'>
-            <Row className='random-planet-bar m-2'>
-                <Col sm={4} className='p-0'>
-                    <Image className='random-planet-widget' src='https://pngimg.com/uploads/starwars/starwars_PNG56.png'></Image>
+            <Row className='random-planet-bar p-3 m-2'>
+                <Col sm={3} className='p-0'>
+                    <Image className='random-planet-image' src='https://pngimg.com/uploads/starwars/starwars_PNG56.png'></Image>
                 </Col>
-                <Col sm={8}>Information ...</Col>
+                <Col sm={4}>
+                    <h1 className='random-planet-name'>The Death Star</h1>
+                </Col>
+                <Col sm={4}>
+                    <Row className='random-planet-description'>
+                        <Col>
+                            <h4>Population: </h4>
+                        </Col>
+                        <Col>
+                            <h4>Climate: </h4>
+                        </Col>
+                    </Row>
+                    <Row className='random-planet-description'>
+                        <Col>
+                            <h4>Diametr: </h4>
+                        </Col>
+                        <Col>
+                            <h4>Gravity: </h4>
+                        </Col>
+                    </Row>
+                    <Row className='random-planet-description'>
+                        <Col>
+                            <h4>Rotation Period:</h4>
+                        </Col>
+                        <Col>
+                            <h4>Orbital Period:</h4>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     )
