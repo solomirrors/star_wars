0e5274c49c193a140b601449f3c872e57f0ad31b
solomirrors <solomirrors@icloud.com>
2021-11-07 03:12:28 +0100

    altered random-person

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -6,13 +6,7 @@ const RandomPlanet = () => {
     return(
         <Container fluid className='p-4 pt-0'>
             <Row className='random-planet-bar p-3 m-2'>
-                <Col className='random-planet-bar-image p-0' md={4}>
-                    <Image className='random-planet-image' src='https://pngimg.com/uploads/starwars/starwars_PNG56.png'></Image>
-                </Col>
-                <Col className='random-planet-bar-name' md={4}>
-                    <h1 className='random-planet-name'>The Death Star</h1>
-                </Col>
-                <Col md={4}>
+                <Col className='random-planet-bar-description' md={4}>
                     <Row className='random-planet-description'>
                         <Col>
                             <h4>Population: </h4>
@@ -37,6 +31,12 @@ const RandomPlanet = () => {
                             <h4>Orbital Period:</h4>
                         </Col>
                     </Row>
+                </Col>
+                <Col className='random-planet-bar-name' md={4}>
+                    <h1 className='random-planet-name'>The Death Star</h1>
+                </Col>
+                <Col className='random-planet-bar-image p-0' md={4}>
+                    <Image className='random-planet-image' src='https://pngimg.com/uploads/starwars/starwars_PNG56.png'></Image>
                 </Col>
             </Row>
         </Container>
